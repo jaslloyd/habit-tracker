4eeaf00c002b61accbe293b9afe4351d069588f6
Jason Lloyd <jason.lloyddev@gmail.com>
2017-10-18 17:50:08 +0100

    Users can now editHabits

--- front-end/src/components/EditHabit.js
+++ front-end/src/components/EditHabit.js
@@ -2,29 +2,87 @@ import React, { Component } from 'react';
 
 class EditHabit extends Component {
     
+    constructor(props){
+        super(props)
+        this.state = {
+            name : '',
+            description: '',
+            category: '',
+            month: '',
+            days: 0
+        }
+        this.id = this.props.match.params.id
+    }
+
+    componentDidMount(){
+        fetch(`http://localhost:3001/api/occurrence_habits/${this.id}`)
+            .then(response => response.json())
+            .then(results => this.setState({
+                name: results.name,
+                description: results.description,
+                category: results.category,
+                month: results.target_month,
+                days: results.target,
+                completed: results.completed
+            }))
+            .catch(e => console.log(e));
+    }
+
+    handleInputChange(e){
+        const {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onSubmit(e){
+        const editedHabit = {
+            'name': this.state.name,
+            'description': this.state.description,
+            'category': this.state.category,
+            'target': parseInt(this.state.days, 10),
+            'completed': this.state.completed,
+            'target_month': this.state.month
+        }
+
+        fetch(`http://localhost:3001/api/occurrence_habits/${this.id}`, {
+            method: 'PUT',
+            headers:{
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(editedHabit)
+          })
+          .then(response => response.json())
+          .then(result => this.props.history.push('/'))
+          .catch(e => console.log(`Failed to edit habit ${e}`));
+
+        e.preventDefault()
+    }
+
     render() {        
         return (
             <div>
                 <h1 className="m-3">Edit Habit</h1>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <div className="form-group">
                         <label htmlFor="habit_name">Habit Name:</label>
-                        <input type="text" className="form-control" ref="habit_name" id="habitName" placeholder="Habit Name e.g. Wake up before 8am each day" required />
+                        <input type="text" className="form-control" name="name" placeholder="Habit Name e.g. Wake up before 8am each day" value={this.state.name} onChange={this.handleInputChange.bind(this)} required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="habit_desc">Description:</label>
-                        <input type="text" className="form-control" ref="habit_desc" id="habit_desc" placeholder="Why do you want to complete it?" required />
+                        <input type="text" className="form-control" name="description" placeholder="Why do you want to complete it?" value={this.state.description} onChange={this.handleInputChange.bind(this)} required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="habit_cat">Category:</label>
-                        <input type="text" className="form-control" ref="habit_cat" id="habit_cat" placeholder="Health / Finance / Career" required />
+                        <input type="text" className="form-control" name="category" placeholder="Health / Finance / Career" onChange={this.handleInputChange.bind(this)} value={this.state.category} required />
                     </div>
                     <div className="form-group">
-                    <label htmlFor="habit_mon">Month of Habit:</label>
+                    <label htmlFor="habit_mon">Month of Habit: {this.state.month}</label>
                     </div>
                     <div className="form-group">
                         <label htmlFor="habit_target">How many days do you want to do this habit?</label>
-                        <input type="number" className="form-control" ref="habit_target" name="days" step="1" min="1" max="30" placeholder="1" />
+                        <input type="number" className="form-control" name="days" step="1" min="1" max="30" value={this.state.days} onChange={this.handleInputChange.bind(this)}  required />
                     </div>
                     <button type="submit" className="btn btn-primary">Update Habit</button>
                 </form>
