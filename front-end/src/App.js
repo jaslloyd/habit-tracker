b3f703722db763e791eca9e2684727b861c17238
thedeployguy <jason.lloyddev@gmail.com>
2018-04-13 19:01:14 +0100

    First checkin for bring in a sidebar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,71 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import Sidebar from 'react-sidebar';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import './App.css';
 
 const App = () => (
   <Fragment>
-    <Navbar />
     <div className="container-fluid">
-      <Main />
+      <div className="row">
+        <div className="col-3 br-1">
+          <h1>Blah</h1>
+        </div>
+        <div className="col-9">
+          <Navbar />
+          <Main />
+        </div>
+      </div>
     </div>
   </Fragment>
 );
-export default App;
+
+class App2 extends Component {
+
+  state = {
+    sidebarOpen: true,
+    styles: {
+      sidebar: {
+        backgroundColor: '#112f3b',
+        width: '225px'
+      }
+    }
+  }
+
+  onSetSidebarOpen = (open) => {
+    this.setState({
+      sidebarOpen: open
+    })
+  }
+
+  render() {
+    var sidebarContent =
+    <div className="mt-2">
+      <img src="http://via.placeholder.com/100x100" alt="" class="img-thumbnail mr-2 ml-2" /  >
+      <ul class="nav flex-column mt-4">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Challenges</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Summary</a>
+          </li>
+      </ul>
+    </div>
+    return (
+      <Sidebar sidebar={sidebarContent}
+               open={this.state.sidebarOpen}
+               docked="true"
+               onSetOpen={this.onSetSidebarOpen}
+               shadow="true"
+               styles={this.state.styles}>
+         <div className="col-12">
+          <Main />
+         </div>
+      </Sidebar>
+    )
+  }
+}
+export default App2;
