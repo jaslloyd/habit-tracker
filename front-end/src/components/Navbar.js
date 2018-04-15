0ff1a2be5fbbf3d59314c20eb909ff07ba90aaa5
thedeployguy <jason.lloyddev@gmail.com>
2018-04-15 10:32:46 +0100

    Fix navbar bug

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -7,18 +7,15 @@ const Navbar = ({openSideBar, sideBarState}) => (
     <button className="custom-button" onClick={() => openSideBar(!sideBarState)}>
         <i className="fas fa-caret-left"></i>
     </button>
-    <nav className="navbar navbar-toggleable-lg navbar-inverse navbar-bg-color navbar-lg">
-      <Link to="/" className="navbar-brand text-white">Habit Tracker</Link>
+    <nav className="navbar navbar-expand-lg navbar-bg-color navbar-lg navbar-dark">
+      <Link to="/" className="navbar-brand">Habit Tracker</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        {/* <ul className="navbar-nav mr-auto">
-          <li className="nav-item active" />
-        </ul> */}
-        <ul className="navbar-nav">
+        <ul className="ml-auto navbar-nav">
           <li className="nav-item">
             {authHelper.isAuthenticated() && <Link to="/login" className="nav-link">Login</Link> }
           </li>
