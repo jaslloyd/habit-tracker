9c16cc698ebf38c993d1ba42bfe3d548ab6c4bfe
thedeployguy <jason.lloyddev@gmail.com>
2018-05-27 21:05:14 +0100

    Add photo to sidebar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,74 +8,93 @@ import './App.css';
 import './BrightTheme.css';
 
 class App extends Component {
-
   state = {
     sidebarState: authHelper.isAuthenticated(),
     styles: {
       sidebar: {
         backgroundColor: '#112f3b',
         width: '225px',
-        boxShadow: '2px 0 10px 0 rgba(0, 0, 0, 0.12), 2px 0 15px 0 rgba(0, 0, 0, 0.09)'
-      }
+        boxShadow: '2px 0 10px 0 rgba(0, 0, 0, 0.12), 2px 0 15px 0 rgba(0, 0, 0, 0.09)',
+      },
     },
-    isDarkTheme: false
-  }
+    isDarkTheme: false,
+  };
 
-  onSetSidebarState = (state) => {
+  onSetSidebarState = state => {
     this.setState({
-      sidebarState: state
-    })
-  }
+      sidebarState: state,
+    });
+  };
 
   onLogout = () => {
-    console.log(this.props.history)
+    console.log(this.props.history);
     authHelper.signout(() => {
       // this.props.history.push('/login')
-    })
-  }
+    });
+  };
 
   toggleTheme = () => {
-    if(!this.state.isDarkTheme === true){
+    if (!this.state.isDarkTheme === true) {
       document.querySelector('body').classList.remove('bright-theme');
     } else {
       document.querySelector('body').classList.add('bright-theme');
     }
-    this.setState({isDarkTheme: !this.state.isDarkTheme})
-  }
+    this.setState({ isDarkTheme: !this.state.isDarkTheme });
+  };
 
   render() {
-    const sidebarContent =
-    <div className="mt-4">
-      <div className="text-center">
-        <img src="http://via.placeholder.com/100x100" alt="" className="img-thumbnail" /  >
+    const sidebarContent = (
+      <div className="mt-4">
+        <div className="text-center">
+          <img
+            src="https://media.licdn.com/dms/image/C5103AQFG1GXNVSdI0w/profile-displayphoto-shrink_200_200/0?e=1533168000&v=beta&t=iPpcTlY_7v4X9xlCuvtujSjRF_6GRHr5lgO_tdDQ1rE"
+            alt=""
+            height="100"
+            width="100"
+            className="img-thumbnail"
+          />
+        </div>
+        <ul className="nav flex-column mt-4">
+          <li className="nav-item">
+            <NavLink activeClassName="active" className="nav-link" to="/dashboard">
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink activeClassName="active" className="nav-link" to="/challenge">
+              Challenges
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink activeClassName="active" className="nav-link" to="/summary">
+              Summary
+            </NavLink>
+          </li>
+        </ul>
       </div>
-      <ul className="nav flex-column mt-4">
-          <li className="nav-item">
-              <NavLink activeClassName='active' className="nav-link" to='/dashboard'>Dashboard</NavLink>
-          </li>
-          <li className="nav-item">
-              <NavLink activeClassName='active' className="nav-link" to='/challenge'>Challenges</NavLink>
-          </li>
-          <li className="nav-item">
-              <NavLink activeClassName='active' className="nav-link" to='/summary'>Summary</NavLink>
-          </li>
-      </ul>
-    </div>
+    );
 
     return (
-      <Sidebar sidebar={sidebarContent}
-               open={this.state.sidebarState}
-               docked={this.state.sidebarState}
-               onSetOpen={this.onSetSidebarState}
-               styles={this.state.styles}>
-        { authHelper.isAuthenticated() &&
-            <Navbar openSideBar={this.onSetSidebarState} sideBarState={this.state.sidebarState} handleLogout={this.onLogout} toggle={this.toggleTheme} />
-        }
-         <div className="col-12">
-            <Main />
-         </div>
+      <Sidebar
+        sidebar={sidebarContent}
+        open={this.state.sidebarState}
+        docked={this.state.sidebarState}
+        onSetOpen={this.onSetSidebarState}
+        styles={this.state.styles}
+      >
+        {authHelper.isAuthenticated() && (
+          <Navbar
+            openSideBar={this.onSetSidebarState}
+            sideBarState={this.state.sidebarState}
+            handleLogout={this.onLogout}
+            toggle={this.toggleTheme}
+          />
+        )}
+        <div className="col-12">
+          <Main />
+        </div>
       </Sidebar>
-    )
+    );
   }
 }
 export default App;
--- front-end/src/components/ChallengeHabit.js
+++ front-end/src/components/ChallengeHabit.js
@@ -7,22 +7,20 @@ import HabitItem from './HabitItem';
 // This class has a lot of duplciate logic from Habit.js, I will continue to get it working then refactor/generalize as do not want to do that to early.
 class ChallengeHabit extends Component {
   state = {
-    displayClass: '',
-  }
+    displayClass: ''
+  };
 
-  onCompleted = (index) => {
+  onCompleted = index => {
     const { id, completed } = this.props.habit;
     if (index <= completed) {
       this.props.onHabitItemUpdated(id, completed - 1);
     } else {
       this.props.onHabitItemUpdated(id, completed + 1);
     }
-  }
+  };
 
   render() {
-    const {
-      id, name, target, completed, lastUpdated, endDate
-    } = this.props.habit;
+    const { id, name, target, completed, lastUpdated, endDate     } = this.props.habit;
     let endDateFormatted;
     let daysLeft;
     // let daysLeftFormatted;
@@ -31,51 +29,67 @@ class ChallengeHabit extends Component {
       endDateFormatted = moment.unix(endDate).format('DD-MM-YYYY');
       daysLeft = moment().diff(moment.unix(endDate), 'days');
       // daysLeftFormatted = Math.abs(daysLeft);
-      if(daysLeft > 0){
+      if (daysLeft > 0) {
         this.setState({ displayClass: 'disabledControls' });
       }
     }
 
-    const habitItemElements = new Array(target).fill().map((_, i) => <HabitItem key={[name, i + 1]} index={i + 1} completed={completed} onCompleted={this.onCompleted} classSettings="big-box" />);
-    const lastUpdatedFormatted = lastUpdated.length > 0 && `${lastUpdated[lastUpdated.length - 1].date}@${lastUpdated[lastUpdated.length - 1].time}`;
+    const habitItemElements = new Array(target)
+      .fill()
+      .map((_, i) => (
+        <HabitItem
+          key={[name, i + 1]}
+          index={i + 1}
+          completed={completed}
+          onCompleted={this.onCompleted}
+          classSettings="big-box"
+        />
+      ));
+    const lastUpdatedFormatted =
+      lastUpdated.length > 0 &&
+      `${lastUpdated[lastUpdated.length - 1].date}@${lastUpdated[lastUpdated.length - 1].time}`;
 
     return (
       <div id={name} key={name} className="card-box col-10 mx-auto pl-0 pr-0 pt-0 pb-0" style={{ lineHeight: '0' }}>
         <div className="row mb-3">
           <div className="col-6">
-            { completed > 0 && <span className="badge badge-pill badge-default challenge-badge">{lastUpdatedFormatted}</span> }
+            {completed > 0 && (
+              <span className="badge badge-pill badge-default challenge-badge">{lastUpdatedFormatted}</span>
+            )}
           </div>
 
           <div className="col-6 text-right">
-            <Link to={`/editHabit/${id}`} type="button" className="btn btn-sm btn-secondary mr-2 text-dark"><i className="far fa-edit"></i></Link>
-            <button type="button" className="btn btn-sm btn-danger" onClick={() => this.props.onDelete(id)}>X</button>
+            <Link to={`/editHabit/${id}`} type="button" className="btn btn-sm btn-secondary mr-2 text-dark">
+              <i className="far fa-edit" />
+            </Link>
+            <button type="button" className="btn btn-sm btn-danger" onClick={() => this.props.onDelete(id)}>
+              X
+            </button>
           </div>
         </div>
 
         <div className="row mb-3" style={{ padding: '0 40px' }}>
           <div className="col-12 card-header">
-            <span className="h5">
-              {name}
-            </span>
+            <span className="h5">{name}</span>
           </div>
         </div>
 
         <div className="row">
-          <div className={'col-md-12 text-center ' + this.state.displayClass} style={{ padding: '0 40px' }}>
+          <div className={`col-md-12 text-center ${this.state.displayClass}`} style={{ padding: '0 40px' }}>
             {habitItemElements}
           </div>
         </div>
 
-
         <div className="row">
-          {
-            endDateFormatted &&
+          {endDateFormatted && (
             <div className="col-6">
               <span className="badge badge-pill badge-default challenge-badge">Complete By: {endDateFormatted}</span>
             </div>
-          }
+          )}
           <div className="ml-auto col-6 text-right">
-            <span className="badge badge-pill badge-default challenge-badge">{completed} / {target}</span>
+            <span className="badge badge-pill badge-default challenge-badge">
+              {completed} / {target}
+            </span>
           </div>
         </div>
       </div>
@@ -89,10 +103,10 @@ ChallengeHabit.propTypes = {
     name: PropTypes.string.isRequired,
     target: PropTypes.number.isRequired,
     completed: PropTypes.number.isRequired,
-    lastUpdated: PropTypes.array.isRequired,
+    lastUpdated: PropTypes.array.isRequired
   }).isRequired,
   onHabitItemUpdated: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired
 };
 
 export default ChallengeHabit;
